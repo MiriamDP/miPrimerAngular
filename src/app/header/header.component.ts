17c1db43a1bb2397733d86d0c68802efe83326c4
Miriam Diaz <miriam101d@gmail.com>
2025-05-15 11:52:38 +0200

    header2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  constructor(){
+    console.log("Constructor ejecutado");
+  }
+  ngOnInit(): void {
+      console.log("On Init ejecutado")
+  }
 }
